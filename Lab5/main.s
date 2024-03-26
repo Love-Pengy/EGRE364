613bc75391c3a55785bcccef6b3773a42fd6ab65
Brandon Frazier <frazierb2@vcu.edu>
2024-03-26 13:38:51 -0400

    code without tests

--- Lab5/main.s
+++ Lab5/main.s
@@ -16,10 +16,10 @@
 	INCLUDE core_cm4_constants.s		; Load Constant Definitions
 	INCLUDE stm32l476xx_constants.s      
 
-; PE12 = A
-; PE13 = B
-; PE14 = C
-; PE15 = D
+;;PB12 = 4
+; PB13 = 3
+;PB14 = 2
+;PB15 = 1
 LED_PIN	EQU	5
 delayAmt DCD 2000
 	
@@ -50,10 +50,10 @@ RCC_Init PROC
 	BX LR ; return from subroutine
 	ENDP
 	
-; PC12-15
+; PB12-15
 GPIO_Init PROC
 	PUSH {R0,R1,R2}
-	LDR R0, =GPIOC_BASE
+	LDR R0, =GPIOB_BASE
 	LDR R1, [R0,#GPIO_MODER]
 	LDR R2, =0x0FF
 	BIC R1,R1,R2, LSL #24
@@ -76,35 +76,79 @@ GPIO_Init PROC
 	BX LR
 	ENDP
 
-;seq1 DCB 2_0101,2_0110,2_1010,2_1001,2_0000
-;seq2 DCB 2_0101,2_1001,2_1010,2_0110,2_0000
-;seq3 DCB 2_0001,2_0100,2_0010,2_1000,2_0000
-;seq4 DCB 2_0001,2_1000,2_0010,2_0100,2_0000
-;seq5 DCB 2_0001,2_0101,2_0100,2_0110,2_0010,2_1010,2_1000,2_1001,2_0000
 
-seq1 DCD 0101,0110,1010,1001,0000 ; full step
-seq2 DCB 0101,1001,1010,0110,0000 ; reverse full step 
-seq3 DCB 0001,0100,0010,1000,0000 ; wave step
-seq4 DCB 0001,1000,0010,0100,0000 ; reverse wave step
-seq5 DCB 0001,0101,0100,0110,0010,1010,1000,1001,0000 ; half step
-seq6 DCB 0001,1001,1000,1010,0010,0110,0100,0101,0000 ; reverse half step
+	
+;2_ in front of these allocates these values in binary
+seq1 DCB 2_0101,2_0110,2_1010,2_1001,2_0000 ; full step
+seq2 DCB 2_0101,2_1001,2_1010,2_0110,2_0000 ; reverse full step 
+seq3 DCB 2_0001,2_0100,2_0010,2_1000,2_0000 ; wave step
+seq4 DCB 2_0001,2_1000,2_0010,2_0100,2_0000 ; reverse wave step
+seq5 DCB 2_0001,2_0101,2_0100,2_0110,2_0010,2_1010,2_1000,2_1001,2_0000 ; half step
+seq6 DCB 2_0001,2_1001,2_1000,2_1010,2_0010,2_0110,2_0100,2_0101,2_0000 ; reverse half step
 
+start_mode DCB 1
 
 
 __main	PROC
+	
+	; Enable the clock to GPIO port C
+	LDR r2, =RCC_BASE
+	LDR r3, [r2, #RCC_AHB2ENR]
+	ORR r3, r3, #RCC_AHB2ENR_GPIOCEN
+	STR r3, [r2, #RCC_AHB2ENR]
+	
+	;; Program GPIOC->MODER to set the mode of Pin PC 13 as input
+	LDR r2, =GPIOC_BASE
+	LDR r3, [r2, #GPIO_MODER]
+	BIC r3, r3, #(3<<(5*(LED_PIN)+1))
+	ORR r3, r3, #0
+	STR r3, [r2, #GPIO_MODER]
+	
+	;Program GPIOC->PUPDR to set the pull-up/pull-down setting of Pin PC 13 as no pull-up no pull-down
+	LDR r2, =GPIOC_BASE
+	LDR r3, [r2, #GPIO_PUPDR]
+	BIC r3, r3, #(3<<(5*(LED_PIN)+1))
+	ORR r3, r3, #0
+	STR r3, [r2, #GPIO_PUPDR]
+	
+	
 	BL RCC_Init ; init RCC
 	BL GPIO_Init ; init GPIO
-	LDR R0,=GPIOC_BASE
+	LDR R0,=GPIOB_BASE
 	ADR R1, seq1; full step
-	MOV R5, R1; move the values of r1 into r5
+	MOV R5, R1; move the address of r1 into r5
+	
+	;checker for full or half step
+	LDR r6, =start_mode
 	
 	
 loop
+	LDR r6, =GPIOC_BASE
+	LDR r7, [r6, #GPIO_IDR]
+	TST r7, #GPIO_IDR_IDR_13
+	BNE switch_mode
+	
 	LDRB R2,[R1],#1; load single byte from r1 into r2 and then increment address
 	CBNZ R2,next; if r2 is 0 go to next 
 	MOV R1, R5 ; sets r1 to the original array
 	B loop
 	
+switch_mode  EOR r6, r6
+	TST r6, #1
+	BNE set_seq_1
+	BEQ set_seq_2
+	B loop
+set_seq_1
+	ADR R1, seq1; full step
+	MOV R5, R1; move the address of r1 into r5
+	B loop
+set_seq_2
+
+	ADR R1, seq5; full step
+	MOV R5, R1; move the address of r1 into r5
+	B loop
+
+
 next
 	LDR R3,[R0,#GPIO_ODR] ; load r3 with value from odr
 	LDR R4,=0x0F
